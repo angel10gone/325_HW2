9189af09c7e185e2b47860b2beef7f56b307a051
angel10gone <113080765+angel10gone@users.noreply.github.com>
2022-09-16 12:49:26 -0400

    Add files via upload

---
+++ comp_pitest.m
@@ -0,0 +1,30 @@
+%test for small number
+pi_est = comp_pi(10);
+a = pi_est;
+a
+
+%test medium number
+pi_est = comp_pi(100);
+b = pi_est;
+assertAccuracy(a, b)
+b
+
+%test large number
+pi_est = comp_pi(1000);
+c = pi_est;
+assertAccuracy(b, c)
+c
+
+%test larget number
+pi_est = comp_pi(100000);
+d = pi_est;
+assertAccuracy(c, d)
+d
+
+%check if previous number was further from pi since more points should mean
+%higher accuracy
+function assertAccuracy(pi_est1, pi_est2)
+diff1 =  abs(pi - pi_est1);
+diff2 = abs(pi - pi_est2);
+assert(diff1 > diff2)
+end
