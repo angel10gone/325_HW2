b1cf65b123e3ea8235858139504b884d797749d9
angel10gone <113080765+angel10gone@users.noreply.github.com>
2022-09-16 17:47:04 -0400

    Update comp_pi.m

--- comp_pi.m
+++ comp_pi.m
@@ -5,7 +5,7 @@ n_inside = 0; %creates variable for # of points inside circle
 for i = 1:n_total
     x = 2*rand()-1; %randomly generates points from -1 to 1
     y = 2*rand()-1;
-    if norm(sqrt(x^2 + y^2)) <= 1
+    if sqrt(x^2 + y^2) <= 1
         n_inside = n_inside+1; %increases number of inside points
     end
 end
