197069cd51472dc2f6175a4528d293e28cd6c3f2
angel10gone <113080765+angel10gone@users.noreply.github.com>
2022-09-17 18:51:04 -0400

    Update modcomp_pi.m

--- modcomp_pi.m
+++ modcomp_pi.m
@@ -1,22 +1,31 @@
+modcomp(1000)
 
-function  pi_est = modcomp_pi(n_total)
+function  pi_est = modcomp(n_total)
 n_inside = 0; %creates variable for # of points inside circle
 hold on
 axis([-1 1 -1 1])
 axis('square')
+video = VideoWriter('modcomp_pi.avi');
+open(video);
 for i = 1:n_total
     x = 2*rand()-1; %randomly generates points from -1 to 1
     y = 2*rand()-1;
     if sqrt(x^2 + y^2) <= 1
-        plot(x, y, 'r.') %plots points red if they're in the circle
+        plot(x, y, 'r.')
         n_inside = n_inside+1; %increases number of inside points
     else
-        plot(x, y, 'c.') %plots points cyan if they're outside of circle
+        plot(x, y, 'c.')
     end
+    drawnow;
+    writeVideo(video, getframe(gcf));
 end
 hold off
-figure; %shows figure
+close(video);
+figure;
 for i = 1:n_inside
     pi_est = 4*i/n_total; %calculates pi_est for all points in n_inside
 end 
 end
+
+
+
