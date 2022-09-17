492db1c3e03a0dccd911afdc1863c53041d355c9
angel10gone <113080765+angel10gone@users.noreply.github.com>
2022-09-17 11:29:08 -0400

    Update modcomp_pi.m

--- modcomp_pi.m
+++ modcomp_pi.m
@@ -8,14 +8,14 @@ for i = 1:n_total
     x = 2*rand()-1; %randomly generates points from -1 to 1
     y = 2*rand()-1;
     if sqrt(x^2 + y^2) <= 1
-        plot(x, y, 'r.')
+        plot(x, y, 'r.') %plots points red if they're in the circle
         n_inside = n_inside+1; %increases number of inside points
     else
-        plot(x, y, 'c.')
+        plot(x, y, 'c.') %plots points cyan if they're outside of circle
     end
 end
 hold off
-figure;
+figure; %shows figure
 for i = 1:n_inside
     pi_est = 4*i/n_total; %calculates pi_est for all points in n_inside
 end 
