a71521443ce6b29df364e645f444431c3fbe9e8e
angel10gone <113080765+angel10gone@users.noreply.github.com>
2022-09-17 10:26:52 -0400

    Update relErrorTest.m

--- relErrorTest.m
+++ relErrorTest.m
@@ -1,15 +1,29 @@
+%tests for high error
 pi_est = relError(0.1);
 a = pi_est;
-a
+a 
 
+%test for medium error
 pi_est = relError(0.01);
 b = pi_est;
 b
+assertAccuracy(a,b);
 
+%test for low error
 pi_est = relError(0.001);
 c = pi_est;
 c
+assertAccuracy(b,c);
 
+%tests lower error
 pi_est = relError(0.0001);
 d = pi_est;
 d
+assertAccuracy(c,d);
+
+%throws error if previous estimation was more accurate than current one
+function assertAccuracy(pi_est1, pi_est2)
+diff1 =  abs(pi - pi_est1);
+diff2 = abs(pi - pi_est2);
+assert(diff1 >= diff2)
+end
